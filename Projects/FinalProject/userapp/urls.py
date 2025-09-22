50bd79a2b00527e12c160c92b3e9dd36e1bbf2c7
SanketTops <sanket.tops@gmail.com>
2025-09-22 10:46:01 +0530

    finalproject

---
+++ Projects/FinalProject/userapp/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path,include
+from userapp import views
+
+urlpatterns = [
+    path('',views.index),
+    path('notes/',views.notes,name='notes'),
+    path('profile/',views.profile,name='profile'),
+    path('about/',views.about,name='about'),
+    path('contact/',views.contact,name='contact'),
+    path('login/',views.login,name='login'),
+    path('signup/',views.signup,name='signup'),
+]
