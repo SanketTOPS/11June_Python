d6bc2ec7c9c38a17dddf9b03c0b457e9d7bc1ce4
SanketTops <sanket.tops@gmail.com>
2025-10-01 10:51:59 +0530

    Custom Admin Panel

---
+++ Projects/FinalProject/adminapp/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path,include
+from adminapp import views
+
+urlpatterns = [
+   path('',views.admin_login),
+   path('admin_dashboard/',views.admin_dashboard,name='admin_dashboard'),
+   path('admin_notesdata/',views.admin_notesdata,name='admin_notesdata'),
+   path('admin_userdata/',views.admin_userdata,name='admin_userdata'),
+]
--- Projects/FinalProject/adminapp/views.py
+++ Projects/FinalProject/adminapp/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from userapp.models import *
 
 # Create your views here.
+def admin_login(request):
+    if request.method=='POST':
+        if request.POST["username"]=="admin" and request.POST["password"]=="tops@123":
+            print("Login Successfull!")
+            return redirect("admin_dashboard")
+        else:
+            print("Error!Login Faild...")
+    return render(request,'admin_login.html')
+
+
+def admin_dashboard(request):
+    userdata=Usersignup.objects.all()
+    u=len(userdata)
+    notesdata=Notes.objects.all()
+    n=len(notesdata)
+    return render(request,'admin_dashboard.html',{'u':u,'n':n,'userdata':userdata})
+
+def admin_userdata(request):
+    userdata=Usersignup.objects.all()
+    return render(request,'admin_userdata.html',{'userdata':userdata})
+
+def admin_notesdata(request):
+    notesdata=Notes.objects.all()
+    return render(request,'admin_notesdata.html',{'notesdata':notesdata})
